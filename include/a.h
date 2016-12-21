f1cbbd7ec87e1bb7ee5612e3b02979aba5f6950e
lhr88899 <229102924@qq.com>
2016-12-21 04:37:09 -0800

    second add

---
+++ include/a.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u08;
---
+++ makefile
@@ -0,0 +1,4 @@
+﻿aa:a.o fun.o fun1.o
+    gcc -o $@ -I. $^
+%.o:%.c
+    gcc -o $@ $^ -I. -c 
